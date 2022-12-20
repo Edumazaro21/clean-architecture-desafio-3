5a7a62e756dca900108dc6d511b3a364cafb36d1
Edumazaro21 <edumazaro21@gmail.com>
2022-12-20 20:47:51 -0300

    Clean architecture desafio 3

---
+++ src/domain/customer/entity/customer.ts
@@ -0,0 +1,80 @@
+import Address from "../value-object/address";
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
+
+export default class Customer extends Entity {
+    private _name: string = "";
+    private _address!: Address;
+    private _active: boolean = false;
+    private _rewardPoints: number = 0;
+
+    constructor(id: string, name: string) {
+        super();
+        this._id = id;
+        this._name = name;
+        this.validate();
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+        }
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get rewardPoints(): number {
+        return this._rewardPoints;
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            this.notification.addError({
+                context: "customer",
+                message: "Id is required",
+            })
+        }
+        if (this._name.length === 0) {
+            this.notification.addError({
+                context: "customer",
+                message: "Name is required",
+            })
+        }
+    }
+
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
+    }
+
+    get address(): Address {
+        return this._address;
+    }
+
+    changeAddress(address: Address) {
+        this._address = address;
+    }
+
+    isActive(): boolean {
+        return this._active;
+    }
+
+    activate() {
+        if (this._address === undefined) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+
+    addRewardPoints(points: number) {
+        this._rewardPoints += points;
+    }
+
+    // tslint:disable-next-line:adjacent-overload-signatures
+    set address(address: Address) {
+        this._address = address;
+    }
+}
